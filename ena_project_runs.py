52491a6595ffff19dd6f985ba97191a5c6cf1396
Berta Gallego <b.gallego@kew.org>
2022-04-05 17:39:52 +0100

    delete unnecessary comments

--- ena_project_runs.py
+++ ena_project_runs.py
@@ -3,57 +3,20 @@ import sys
 import xml.etree.ElementTree as ET
 import re
 import csv
-"""
-Input: ENA project accession
-Output: a CSV with the project accession as a name and two columns: 
-    the ENA Run Id and the submitters Id.
-    get_all_runs(project accession)
-        project_tree = get_xml(project_accession)
-        project_runs = get_runs_in_project(project_tree)
-        project_runs_and_submitter_ids = get_submitters_id(project_runs)
-        save_to_csv(project_runs_and_submitter_ids)
 
-    -------
-
-    get_xml(string: project_accession): xml tree object
-    {Uses the ENA API to request an xml with the project data}
-
-    get_runs_in_project(xml tree: project_tree): dictionary 
-        runs_url = get_submitted_files(project_tree)
-        return get_runs_in_table(runs_url)
-
-    get_submitters_id(inout dictionaty: project_runs): dictionary
-        for run in project_runs:
-            submitter_id = get_submitter_id(run)
-            add_submitter_id(submitter_id)
-        return project_runs
-    
-    save_to_csv(dictionary: project_runs_and_submitters_ids)
-        {Save dictionary to CSV}
-
-    -------
-
-    get_submitted_files(xml tree object: project_tree): string
-    {Get URL with the run accessions and file names in the project}
-
-    get_runs_in_table(runs_url): dictionary
-    {Parse the first column "run_accession" in txt
-    and create a dictionary
-    {project_accession: {ena_run_1, 
-                         ena_run_2, 
-                         ..., 
-                         ena_run_n}
-    }}
-
-"""
 def get_all_runs(project_accession):
+    """
+    Given an ENA project accession
+    return a CSV with the run accessions and their
+    correspondint submitters Id.
+    """
     project_tree = get_xml(project_accession)
     runs_url = get_url_submitted_files(project_tree)
     runs_list =  get_runs_in_table(runs_url)
     accessions = {}
     accessions["project"] = project_accession
     accessions["runs"] = {}
-    for run_accession in runs_list[:5]:
+    for run_accession in runs_list:
         submitters_id = get_submitters_id(run_accession)
         accessions["runs"][run_accession] = submitters_id
     save_to_csv(accessions)

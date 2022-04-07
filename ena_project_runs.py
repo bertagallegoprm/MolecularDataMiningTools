4faff646a713ffa2dbcbc5573cde216c884fa4dc
Berta Gallego <b.gallego@kew.org>
2022-04-07 12:24:04 +0100

    add docstrings

--- ena_project_runs.py
+++ ena_project_runs.py
@@ -1,3 +1,11 @@
+"""
+Author: Berta Gallego
+Last reviewed: 2022-04-07
+Usage: python3 ena_project_runs.py -p <project accession>
+Description: Given an ENA project accession
+             return a CSV with the run accessions and their
+             corresponding submitters ID.
+"""
 import requests
 import sys
 import xml.etree.ElementTree as ET
@@ -9,7 +17,7 @@ def get_all_runs(project_accession):
     """
     Given an ENA project accession
     return a CSV with the run accessions and their
-    correspondint submitters Id.
+    corresponding submitters ID.
     """
     print(f"Getting run accessions from {project_accession}...")
     project_tree = get_xml(project_accession)
@@ -18,7 +26,7 @@ def get_all_runs(project_accession):
     accessions = {}
     accessions["project"] = project_accession
     accessions["runs"] = {}
-    print("Getting submitters Ids from run accessions...")
+    print("Getting submitters IDs from run accessions...")
     for run_accession in runs_list:
         submitters_id = get_submitters_id(run_accession)
         accessions["runs"][run_accession] = submitters_id
@@ -49,6 +57,13 @@ def get_url_submitted_files(project_tree):
             return item.find("XREF_LINK/ID").text
 
 def get_runs_in_table(url):
+    """
+    Return a list with the run accessions
+    in the table linked in ENA-SUBMITTED-FILES tag URL,
+    in the project XML.
+    This may need to be adapted to other tables, 
+    where all the columns have data.
+    """
     response = requests.get(f"{url}")
     assert response.status_code == 200, f"[ERROR]: Unable to access {url}"
     raw_data = response.content.decode("utf/8")
@@ -57,10 +72,20 @@ def get_runs_in_table(url):
     return re.split("\t\t\t\t\n", raw_data.strip())
 
 def get_submitters_id(run_accession):
+    """
+    Return the submitters ID in the run XML
+    given a run accession.
+    """
     run_tree = get_xml(run_accession)
     return run_tree.find("RUN/EXPERIMENT_REF/IDENTIFIERS/SUBMITTER_ID").text
 
 def save_to_csv(accessions):
+    """
+    Save a CSV with two columns: 
+        - run_accession
+        - submitters_id
+    given a dictionary with the accession data.
+    """
     project = accessions["project"]
     headers = ["run_accession", "submitters_id"]
     with open(f"{project}_accessions.csv", "w") as f:
@@ -71,7 +96,7 @@ def save_to_csv(accessions):
         f.close()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog = "Get INSDC run accessions and submitters Ids given a project accession", usage = "python3 ena_project_runs.py -p <project accession>")
+    parser = argparse.ArgumentParser(prog = "Get INSDC run accessions and submitters IDs given a project accession", usage = "python3 ena_project_runs.py -p <project accession>")
     parser.add_argument("-p", "--project", type=str,  help = "INSDC project accession (e.g. PRJNA693894")
     args = parser.parse_args()
     get_all_runs(args.project)
